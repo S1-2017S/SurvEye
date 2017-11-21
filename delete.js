eba0dfd08cbaadb6042af82541cf3ebe7f3f1e3c
PirateSeal <PirateSeal@users.noreply.github.com>
2017-11-21 09:31:47 +0100

    Ajout option supprimer sondage

--- delete.js
+++ delete.js
@@ -15,25 +15,26 @@ var del = function (req,res,query) {
 	var i;
 	var j;
 
+	page = fs.readFileSync("./res_confirm_delete_sondage.html","utf-8");
+
 	contenu_fichier = fs.readFileSync("./profils.JSON","utf-8");
 	contenu_fichier = JSON.parse(contenu_fichier);
-	console.log(contenu_fichier);
-	
+
 	i = 0;
-	while (contenu_fichier[i] != query.id && i<contenu_fichier.length) {
+	while (contenu_fichier[i].id != query.id && i<contenu_fichier.length) {
 		i++
 	}
 
 	for (j = 0; j < contenu_fichier[i].sondageuser.length; j++) {
 		if (query.sondage === contenu_fichier[i].sondageuser[j]) {
-			contenu_fichier.sondageuser.splice(j, 1);
+			contenu_fichier[i].sondageuser.splice(j, 1);
 		}
 	}
 
 
 	contenu_fichier = JSON.stringify(contenu_fichier);
 	fs.writeFileSync("./profils.JSON",contenu_fichier,"utf-8");
-	page = fs.readFileSync("./res_confirm_action_sondage.html","utf-8");
+	page = fs.readFileSync("./res_confirm_delete_sondage.html","utf-8");
 
 	marqueurs = {};
 	marqueurs.confirm = "supprimé";
