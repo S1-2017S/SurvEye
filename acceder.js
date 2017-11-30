59c3ce4ef70b1bcda3b4d98c67412a62b78faed5
Olivier <patrice.thire@esiea.fr>
2017-11-30 10:15:27 +0100

    Condition d'ouverture du sondage gérée

--- acceder.js
+++ acceder.js
@@ -22,17 +22,53 @@ var trait = function(req,res,query) {
     contenu_fichier = fs.readFileSync("./"+query.sondage+".json", "utf-8");
     contenu_fichier = JSON.parse(contenu_fichier);
 
-    i = 0;
-    trouve = false;
-    while(trouve === false && i < contenu_fichier.ids.length) {
-        if(contenu_fichier.ids[i] === query.id) {
-            trouve = true;
-        }else {
-            i++;
-        }
-    }
+    if(contenu_fichier.etat === "open") {
 
-    if(trouve === true) {
+        i = 0;
+        trouve = false;
+        while(trouve === false && i < contenu_fichier.ids.length) {
+            if(contenu_fichier.ids[i] === query.id) {
+                trouve = true;
+            }else {
+                i++;
+            }
+        }
+
+        if(trouve === true) {
+            page = fs.readFileSync("./res_resultats_sondages.html", "utf-8");
+            marqueurs = {};
+            marqueurs.id = query.id;
+            marqueurs.sondage = "";
+            nb_reponses = 0;
+            for(i = 0; i < contenu_fichier.answers.length; i++) {
+                marqueurs.sondage += "<h2>"+contenu_fichier.questions[i]+"</h2><br>";
+                for(x = 0; x < contenu_fichier.answers[i].length; x++) {
+                    if(contenu_fichier.answers[i][x] !== 0) {
+                        nb_reponses += contenu_fichier.answers[i][x];
+                    }
+                }
+                for(x = 0; x < contenu_fichier.answers[i].length; x++) {
+                    console.log(contenu_fichier.reponses[i][x]);
+                    marqueurs.sondage += contenu_fichier.reponses[i][x]+"<img src='./barre_histo.PNG' style=' height : 20px; width : "+(contenu_fichier.answers[i][x]/nb_reponses)*100+"%' alt="+(contenu_fichier.answers[i][x]/nb_reponses)*100+"%><br>";
+                }
+            }
+            page = page.supplant(marqueurs);
+        
+        }else if(trouve === false) {
+            page = fs.readFileSync("./res_reponse_sondage.html", "utf-8");
+            marqueurs = {};
+            marqueurs.id = query.id;
+            marqueurs.questions = "";
+            marqueurs.sondage = query.sondage;
+            for(i = 0; i < contenu_fichier.questions.length; i++) {
+                marqueurs.questions += "<h2>Question "+(i+1)+" : "+contenu_fichier.questions[i]+"</h2><br>"
+                for(x = 0; x < contenu_fichier.reponses[i].length; x++) {
+                    marqueurs.questions += "<input type='radio' name='q"+i+"' value='"+x+"'>"+contenu_fichier.reponses[i][x]+"<br>"
+                }
+            }
+            page = page.supplant(marqueurs);
+        }
+    }else {
         page = fs.readFileSync("./res_resultats_sondages.html", "utf-8");
         marqueurs = {};
         marqueurs.id = query.id;
@@ -50,24 +86,11 @@ var trait = function(req,res,query) {
                 marqueurs.sondage += contenu_fichier.reponses[i][x]+"<img src='./barre_histo.PNG' style=' height : 20px; width : "+(contenu_fichier.answers[i][x]/nb_reponses)*100+"%' alt="+(contenu_fichier.answers[i][x]/nb_reponses)*100+"%><br>";
             }
         }
+        
         page = page.supplant(marqueurs);
-    
-    }else if(trouve === false) {
-        page = fs.readFileSync("./res_reponse_sondage.html", "utf-8");
-        marqueurs = {};
-        marqueurs.id = query.id;
-        marqueurs.questions = "";
-        marqueurs.sondage = query.sondage;
-        for(i = 0; i < contenu_fichier.questions.length; i++) {
-            marqueurs.questions += "<h2>Question "+(i+1)+" : "+contenu_fichier.questions[i]+"</h2><br>"
-            for(x = 0; x < contenu_fichier.reponses[i].length; x++) {
-                marqueurs.questions += "<input type='radio' name='q"+i+"' value='"+x+"'>"+contenu_fichier.reponses[i][x]+"<br>"
-            }
-        }
-        page = page.supplant(marqueurs);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(page);
+        res.end();
     }
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(page);
-    res.end();
 }
 module.exports = trait;
