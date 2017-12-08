b31b12c9e64f807ba86d718c0eb913f233631a8b
Olivier <o.lespagnon@gmail.com>
2017-12-08 11:47:39 +0100

    Modification des questions par l'historique

--- valider.js
+++ valider.js
@@ -10,29 +10,30 @@ var fs = require("fs");
 require("remedial");
 
 var histo = function(req,res,query) {
-    var page;
-    var marqueurs;
-    var contenu_fichier;
-    var question;
-    var reponses;
-    var i;
+	var page;
+	var marqueurs;
+	var contenu_fichier;
+	var question;
+	var reponses;
+	var i;
+	var answers;
 	var compteur;
 	var url = require("url");
-	
+
 	page = fs.readFileSync("./res_creation_sondage.html", "utf-8");
 
-    contenu_fichier = fs.readFileSync("./"+query.id+"t.json", 'utf-8');
-    contenu_fichier = JSON.parse(contenu_fichier);
+	contenu_fichier = fs.readFileSync("./"+query.id+"t.json", 'utf-8');
+	contenu_fichier = JSON.parse(contenu_fichier);
 
-    marqueurs = {};
-    marqueurs.id = query.id;
-    question = query.q;
-    reponses = [];
+	marqueurs = {};
+	marqueurs.id = query.id;
+	question = query.q;
+	reponses = [];
 	marqueurs.erreurQ = "";
 	marqueurs.erreurR = "";
 	compteur = 0;
 	marqueurs.histo = "";
-	
+
 	for(i = 0; i < 10; i ++) {
 		if(query[String(i)] !== "") {
 			reponses.push(query[String(i)]);
@@ -40,11 +41,11 @@ var histo = function(req,res,query) {
 		}
 	}
 
-    if(query.q === "") {
+	if(query.q === "") {
 		marqueurs.erreurQ = "Vous devez mettre une question";
 		marqueurs.question = "Question "+(contenu_fichier.questions.length+1);
 	} else if(compteur < 2) {
-		marqueurs.erreurR = "Vous devez mettre au minimum 2 réponses"
+		marqueurs.erreurR = "Vous devez mettre au minimum 2 réponses";
 		marqueurs.question = "Question "+(contenu_fichier.questions.length+1);
 	} else {
 		if(contenu_fichier.questions[query.numero] !== "") {
@@ -55,14 +56,13 @@ var histo = function(req,res,query) {
 			contenu_fichier.reponses.push(reponses);
 			contenu_fichier.answers.push([]);
 		}
-		
-		i = 0;
-		while(i < reponses.length) {
-			 if(query[String(i)] !== "") {
-			 	contenu_fichier.answers[contenu_fichier.questions.length-1].push(0);
+		answers = [];
+		for(i = 0; i<10; i++) {
+			if(query[String(i)] !== "") {
+				answers.push(0);
 			}
-			i++;
 		}
+		contenu_fichier.answers[query.numero] = answers;
 		marqueurs.question = "Question "+(contenu_fichier.questions.length+1);
 
 		for(i = 0; i < contenu_fichier.questions.length; i++) {
@@ -72,14 +72,14 @@ var histo = function(req,res,query) {
 		contenu_fichier = JSON.stringify(contenu_fichier);
 		fs.writeFileSync("./"+query.id+"t.json", contenu_fichier, "utf-8");
 	}
-	
+
 	marqueurs.q = "";
 	for(i = 0; i < 10; i++) {
 		marqueurs[String(i)] = "";
 	}
-	
+
 	page = page.supplant(marqueurs);
-    res.writeHead(200, {'Content-Type': 'text/html'});
+	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.write(page);
 	res.end();
 
