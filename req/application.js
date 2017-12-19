13b0de3ccdbdbb745b06855fde92ef98a12b5088
Olivier <o.lespagnon@gmail.com>
2017-12-19 14:34:30 +0100

    Ajout du focus à l'ouverture de la creation

--- req/application.js
+++ req/application.js
@@ -7,12 +7,9 @@ function closeNav() {
 	document.getElementById("mySidepannel").style.width = "0px";
 }
 var trait = function() {
-	var x = document.getElementById("opener");
-	x.addEventListener("click", openNav);
+	document.getElementById("opener").addEventListener("click", openNav);
 	document.getElementById("closer").addEventListener("click", closeNav);
-	document.getElementsByName("retour").addEventListener("click", function() {
-		confirm("Voulez-vous abandonner ?");
-	});
+	document.getElementById("q").focus();
 }
 
 window.addEventListener("load", trait);
