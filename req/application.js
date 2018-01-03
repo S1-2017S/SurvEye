3552cd7bc20cdd68ab80ad7b175237e0c92895a9
Olivier <o.lespagnon@gmail.com>
2018-01-03 09:22:50 +0100

    Plus de bootstrap

--- req/application.js
+++ req/application.js
@@ -1,11 +1,16 @@
 "use strict"
 
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
     document.getElementById("mySidepannel").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
 }
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
-	document.getElementById("mySidepannel").style.width = "0px";
-}
+    document.getElementById("mySidepannel").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+} 
 var trait = function() {
 	document.getElementById("opener").addEventListener("click", openNav);
 	document.getElementById("closer").addEventListener("click", closeNav);
